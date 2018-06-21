0f6463dfee178d38a466d9dadf184afab9bec3d6
parth-gulati <gulati.parth98@gmail.com>
2018-06-21 20:50:24 +0530

    Update SendMailSSL.java

--- SendMailSSL.java
+++ SendMailSSL.java
@@ -5,7 +5,7 @@ import java.text.ParseException;
 public class SendMailSSL{    
 		 public static void send1(String fdate) {    
 		     //calling send function in Mailer class 
-		     Mailer.send("internship.sample1998","Sample@123","gulati.parth98@gmail.com","Meeting Notification","Good morning sir\nHere are the details for your meeting: \n\n  Date and time :" + fdate + "\n\nPlease be online on the given time and date for the meeting to take place. \n\n Thanks,\nParth");  
+		     Mailer.send("<enter sender's email id>","<password>","<enter receiver's email id>","Meeting Notification","Good morning sir\nHere are the details for your meeting: \n\n  Date and time :" + fdate + "\n\nPlease be online on the given time and date for the meeting to take place. \n\n Thanks,\nParth");  
 		        
 		 }   
 }
